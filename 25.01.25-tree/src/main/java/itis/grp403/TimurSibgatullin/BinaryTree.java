6c866849120303592012370d0d2b048f3e44b3c3
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-01-29 13:22:20 +0300

    minor changes tree

--- 25.01.25-tree/src/main/java/itis/grp403/TimurSibgatullin/BinaryTree.java
+++ 25.01.25-tree/src/main/java/itis/grp403/TimurSibgatullin/BinaryTree.java
@@ -1,10 +1,19 @@
 package itis.grp403.TimurSibgatullin;
 
 public abstract class BinaryTree {
+    public Node node;
+
     protected Node traversalTree(Node node) {
         return null;
     }
 
+    public BinaryTree(int[] values) {
+        Node res = new Node(values[0]);
+        for (int i = 1; i < values.length; i++) {
+            addLeaf(values[i], res);
+        }
+        this.node = res;
+    }
 
     static private void addLeaf(int value, Node node) {
         if (value > node.value()) {
@@ -22,13 +31,5 @@ public abstract class BinaryTree {
                 addLeaf(value, node.left());
             }
         }
-    }
-
-     protected Node growTree(int[] values) {
-        Node res = new Node(values[0]);
-        for (int i = 1; i < values.length; i++) {
-            addLeaf(values[i], res);
-        }
-        return res;
     }
 }
