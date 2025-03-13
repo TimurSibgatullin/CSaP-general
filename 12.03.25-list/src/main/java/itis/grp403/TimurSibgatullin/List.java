9950e00acb4ec2e7571cde78813a459f1c28fcb7
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-03-13 09:59:22 +0300

    list

---
+++ 12.03.25-list/src/main/java/itis/grp403/TimurSibgatullin/List.java
@@ -0,0 +1,84 @@
+package itis.grp403.TimurSibgatullin;
+
+public class List implements ListInterface {
+    private Integer[] vals;
+    private int size;
+
+    public List() {
+        this.vals = new Integer[10];
+        this.size = 0;
+    }
+
+    public void add(Integer element) {
+        if (size >= vals.length) {
+            grow();
+        }
+        vals[size++] = element;
+    }
+
+    public void add(Integer element, int position) {
+        if (position > size || position < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (size == vals.length) {
+            grow();
+        }
+        for (int i = size; i != position; i--) {
+            vals[i] = vals[i-1];
+        }
+        vals[position] = element;
+        size++;
+
+    }
+
+    public void grow() {
+        Integer[] temp = new Integer[(int) (size * 1.5)];
+        for (int i = 0; i < size; i++) {
+            temp[i] = vals[i];
+        }
+        vals = temp;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Integer get(int position) throws IndexOutOfBoundsException {
+        if (position > size - 1 || position < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        return vals[position];
+    }
+
+    public void sort(boolean asc) {
+        for (int i = 0; i < (size - 1); i++) {
+            for (int j = 0; j < (size - 1); j++) {
+                    if (((vals[j] > vals[j+1]) && asc) || ((vals[j] < vals[j+1]) && !asc)) {
+                        Integer temp = vals[j+1];
+                        vals[j+1] = vals[j];
+                        vals[j] = temp;
+                }
+            }
+        }
+    }
+
+    public Integer remove(int position) throws IndexOutOfBoundsException {
+        if (position > size-1 || position < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+        Integer temp = vals[position];
+        for (int i = position; i < size - 1; i++) {
+            vals[i] = vals[i + 1];
+        }
+        size--;
+        return temp;
+    }
+
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < size; i++) {
+            res += vals[i] + " ";
+        }
+        return res;
+    }
+}
---
+++ 12.03.25-list/src/main/java/itis/grp403/TimurSibgatullin/ListInterface.java
@@ -0,0 +1,40 @@
+package itis.grp403.TimurSibgatullin;
+
+public interface ListInterface {
+    /**
+     * добавляет в конец списка
+     * @param element
+     */
+    void add(Integer element);
+
+    /**
+     * метод добавляет элемент в указаннную позицию
+     * @param element
+     */
+    void add(Integer element, int position);
+
+    /**
+     * возвращает размер списка
+     * @return
+     */
+    int size();
+
+    /**
+     * возвращает элемент по индексу
+     * @param position
+     * @return
+     */
+    Integer get(int position) throws IndexOutOfBoundsException;
+
+    /**
+     * Сортировка списка (asc = true -- по возрастанию)
+     */
+    void sort(boolean asc);
+
+    /**
+     * Удаляет элемент по индексу
+     * @param position
+     * @throws IndexOutOfBoundsException
+     */
+    Integer remove(int position) throws IndexOutOfBoundsException;
+}
