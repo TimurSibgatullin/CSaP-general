68d333c1e7595bf6924bf3a82801058886d3a68d
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-03-21 22:23:16 +0300

    linked vs list

--- 15.03.25-generic-set/src/main/java/itis/grp403/TimurSibgatullin/AppleTest.java
+++ 15.03.25-generic-set/src/main/java/itis/grp403/TimurSibgatullin/AppleTest.java
@@ -4,7 +4,7 @@ public class AppleTest {
     public static void main(String[] arg) {
         Set<Apple> set1 = new Set<>();
         set1.add(new Apple("Красный", 55, "Антоновка", 120));
-        set1.add(new Apple("Зеленый", 60, "Антоновка", 100));
+        set1.add(new Apple("Зеленый", 60, "Анто+новка", 100));
         set1.add(new Apple("Желтый", 80, "Голден", 130));
         set1.add(new Apple("Алый", 75, "Гала", 150));
         set1.add(new Apple("Сливовый", 70, "Спартан", 90));
