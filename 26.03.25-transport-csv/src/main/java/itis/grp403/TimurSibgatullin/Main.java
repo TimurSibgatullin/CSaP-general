a6a6e36b1bdb7abd99649fee641c9416bb70c800
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-03-29 15:22:54 +0300

    transport-csv

--- 26.03.25-transport-csv/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ 26.03.25-transport-csv/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -6,12 +6,36 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
+//        1. Описать базовый класс Transport
+//        2. Описать потомков Bus, Tram, Trolleybus
+//        3. Реализовать методы
+//        - Чтение из файла в список Transport
+//                - Чтение из файла в списки по типам
+        //        - По списку вывести в упорядоченном виде парковые номера и время начала движения
+//                - По списку вывести в упорядоченном виде номера маршрутов, время начала и окончнания движения маршрута
+//                - По списку вывести кол-во разных маршрутов
+        //        - По списку вывести кол-во разных транспортных средств
+//                - По списку вывести номер маршрута и кол-во транспортных средств на нем
+
         TransportService transportService = new TransportService("C:\\Users\\timur\\IdeaProjects\\CSaP-general\\26.03.25-transport-csv\\transport.csv");
         List<Transport> transports = transportService.readAll();
-//        System.out.println(transports.toString());
-//        TransportPrintService transportPrintService = new TransportPrintService();
-//        transportPrintService.printParkNumber(transports);
+        System.out.println(transports);
+
+        List<Bus> buses = transportService.readAllBuses();
+        System.out.println(buses);
+
+        List<Tram> trams = transportService.readAllTrams();
+        System.out.println(trams);
+
+        List<Trolleybus> trolleybuses = transportService.readAllTrolleys();
+        System.out.println(trolleybuses);
+
         TransportPrintService transportPrintService2 = new TransportPrintService();
-        transportPrintService2.printParkNumber2(transports);
+        transportPrintService2.printSortedParkNumber(transports);
+
+        TransportPrintService transportPrintService3 = new TransportPrintService();
+        transportPrintService3.printSortedRouteNumber(transports);
+
+
     }
 }
--- 26.03.25-transport-csv/src/main/java/itis/grp403/TimurSibgatullin/TransportPrintService.java
+++ 26.03.25-transport-csv/src/main/java/itis/grp403/TimurSibgatullin/TransportPrintService.java
@@ -11,7 +11,7 @@ public class TransportPrintService {
         }
     }
 
-    public <T extends Transport> void printParkNumber2(List<T> list) {
+    public <T extends Transport> void printSortedParkNumber(List<T> list) {
         list.sort(new Comparator<T>() {
             @Override
             public int compare(Transport o1, Transport o2) {
@@ -19,7 +19,29 @@ public class TransportPrintService {
             }
         });
         for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).getNumber() + " " + list.get(i).getWorkTimeBegin());
+            System.out.print(list.get(i).getNumber() + " " + list.get(i).getWorkTimeBegin() + "\t");
         }
+        System.out.println();
+    }
+
+    public <T extends Transport> void printSortedRouteNumber(List<T> list) {
+        list.sort(new Comparator<T>() {
+            @Override
+            public int compare(Transport o1, Transport o2) {
+                return o1.getRouteNumber().compareTo(o2.getRouteNumber());
+            }
+        });
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i).getRouteNumber() + " " + list.get(i).getWorkTimeBegin() + " " + list.get(i).getWorkTimeEnd() + "\t");
+        }
+        System.out.println();
+    }
+
+    public void RoutesCounter(List<Transport> transport) {
+        Set<String> routes = new Set<>();
+        for (int i = 0; i < transport.size(); i++) {
+            routes.add(transport.get(i).getRouteNumber());
+        }
+        System.out.println(routes.size());
     }
 }
