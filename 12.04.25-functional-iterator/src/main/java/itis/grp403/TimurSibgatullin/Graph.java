3c14761f95213d42b89d73b505f8ae2ace65528f
Timur Sibgatullin <trsibgatullin@kpfu.ru>
2025-04-16 10:51:40 +0300

    hashmap iterator

--- 12.04.25-functional-iterator/src/main/java/itis/grp403/TimurSibgatullin/Graph.java
+++ 12.04.25-functional-iterator/src/main/java/itis/grp403/TimurSibgatullin/Graph.java
@@ -4,48 +4,65 @@ import org.w3c.dom.ls.LSOutput;
 
 import java.util.*;
 
-public class Graph implements Iterable<HashMap>{
+public class Graph implements Iterable<Character>{
     private Map<Character, Set<Character>> graph;
 
     public Graph() {
         graph = new HashMap<>();
 
-        graph.put('d', null);
+        graph.put('d', new HashSet<>());
         graph.put('a', new HashSet<>(Set.of('d','c')));
         graph.put('f', new HashSet<>(Set.of('d')));
         graph.put('g', new HashSet<>(Set.of('d','c', 'f')));
-        graph.put('c', null);
+        graph.put('c', new HashSet<>());
         graph.put('b', new HashSet<>(Set.of('f','g','a','c')));
     }
 
     public Set<Character> sort() {
-        return null;
+        Set<Character> result = new LinkedHashSet<>();
+        while (!graph.isEmpty()) {
+            // ищем вершины без зависимостей
+            Character nodeToAdd = null;
+            for (Map.Entry<Character, Set<Character>> entry : graph.entrySet()) {
+                if (entry.getValue().isEmpty()) {
+                    nodeToAdd = entry.getKey();
+                    break;
+                }
+            }
+            // если нет вершин с нулевыми зависимостями
+            if (nodeToAdd == null) {
+                throw new RuntimeException("Цикл в графе — сортировка невозможна");
+            }
+
+            // добавляем в результат вершину с нулевыми зависимостями
+            result.add(nodeToAdd);
+            // удаляем из графа вершину с нулевыми зависимостями
+            graph.remove(nodeToAdd);
+
+            // удаляем зависимости
+            for (Set<Character> deps : graph.values()) {
+                deps.remove(nodeToAdd);
+            }
+        }
+        return result;
     }
 
     @Override
-    public Iterator<HashMap> iterator() {
+    public Iterator<Character> iterator() {
         return new GraphIterator();
     }
 
-    class GraphIterator implements Iterator<HashMap> {
+    class GraphIterator implements Iterator<Character> {
         int currentCount = 0;
-        HashMap<Character> result = new HashMap<>();
+        Character[] result = sort().toArray(Character[]::new);
         @Override
         public boolean hasNext() {
-            return !graph.isEmpty();
+            return (currentCount < result.length);
         };
 
         @Override
-        public HashMap next() {
-            for (Character i : graph.keySet()) {
-                if (graph.get(i) == null) {
-                    result.add(i);
-                    for (Character j : graph.keySet()) {
-                        graph.get(j).remove(i);
-                    }
-                }
-            }
-            return result;
+        public Character next() {
+            return result[currentCount++];
         }
     }
 }
--- 12.04.25-functional-iterator/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ 12.04.25-functional-iterator/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -3,8 +3,10 @@ package itis.grp403.TimurSibgatullin;
 import java.util.Set;
 
 public class Main {
-    Graph graph1 = new Graph();
-    for (Set<Character> i : graph1) {
-        System.out.println(i);
+    public static void main(String[] args) {
+        Graph graph1 = new Graph();
+        for (Character i : graph1) {
+            System.out.println(i);
+        }
     }
 }
