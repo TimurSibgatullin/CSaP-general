9a8c668e7c65222b45313be844de9b38798466ce
Timur Sibgatullin <timur.sibgatullin.26@mail.ru>
2025-05-21 15:19:28 +0300

    db homework

--- lab2_12_db/src/main/java/itis/grp403/TimurSibgatullin/Main.java
+++ lab2_12_db/src/main/java/itis/grp403/TimurSibgatullin/Main.java
@@ -3,26 +3,17 @@ package itis.grp403.TimurSibgatullin;
 public class Main {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setName("Artem");
-        student.setLastName("Kuzmin");
-        student.setFatherName("Ruslanovich");
+        student.setName("Юлиан");
+        student.setLastName("Семенов");
+        student.setFatherName("Цезаревич");
         student.setGroup("11-403");
-        student.setId(14);
-
-        DBUtils.appendObject(student);
-
-        Student student2 = new Student();
-        student2.setName("Юлиан");
-        student2.setLastName("Семенов");
-        student2.setFatherName("Цезаревич");
-        student2.setGroup("11-403");
-        student2.setId(42);
-
-        DBUtils.appendObject(student2);
+        student.setId(45);
         DBUtils.editStudent(student);
 
         DBUtils.readAll().forEach(s -> {
-            System.out.println(s.getId() + " " +  s.getName());
+            System.out.println(s.getId() + " " + s.getName() + " " + s.getLastName());
         });
+
+
     }
 }
