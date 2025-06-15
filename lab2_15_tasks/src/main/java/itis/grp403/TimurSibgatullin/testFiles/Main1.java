e799d5cdaaff50c6dfe87fb1c69545adc0f94a60
Timur_Sibgatullin <timur.sibgatullin.26@mail.ru>
2025-06-15 23:42:31 +0300

    IO tests

--- lab2_15_tasks/src/main/java/itis/grp403/TimurSibgatullin/testFiles/Main1.java
+++ lab2_15_tasks/src/main/java/itis/grp403/TimurSibgatullin/testFiles/Main1.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main1 {
-
     public static byte[] readFileToByteArray(String filePath) throws IOException {
         try (FileInputStream fis = new FileInputStream(filePath)) {
             byte[] buffer = new byte[fis.available()];
@@ -15,14 +14,11 @@ public class Main1 {
     }
 
     public static byte[] countingSort(byte[] array) {
-        int[] count = new int[256]; // Массив для хранения частот байтов от 0 до 255
-
-        // Подсчитываем частоту каждого байта
+        int[] count = new int[256];
         for (byte b : array) {
             count[b & 0xFF]++;
         }
 
-        // Создаем отсортированный массив
         byte[] sortedArray = new byte[array.length];
         int index = 0;
         for (int i = 0; i < count.length; i++) {
@@ -42,14 +38,14 @@ public class Main1 {
     }
 
     public static void main(String[] args) {
-        String inputFilePath = "input.txt"; // Путь к входному файлу
-        String outputFilePath = "sorted_output.txt"; // Путь к выходному файлу
+        String inputFilePath = "input.txt";
+        String outputFilePath = "sorted_output.txt";
 
         try {
             byte[] fileBytes = readFileToByteArray(inputFilePath);
             byte[] sortedBytes = countingSort(fileBytes);
             writeByteArrayToFile(sortedBytes, outputFilePath);
-            System.out.println("Файл успешно отсортирован и сохранен.");
+            System.out.println("done");
         } catch (IOException e) {
             e.printStackTrace();
         }
